be8b5ef0c38e7c7687d3e1dd246b825f82a795c5
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2017-10-04 20:00:03 +0200

    ConditionOnMissingBean example

--- src/main/java/com/example/restdemo/RestdemoApplication.java
+++ src/main/java/com/example/restdemo/RestdemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.restdemo;
 
+import com.example.restdemo.service.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestdemoApplication.class, args);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public HelloService helloService() {
+		return new HelloService() {
+			@Override
+			public String sayHello() {
+				return "You don't have bean - I'm default";
+			}
+		};
 	}
 
 }
