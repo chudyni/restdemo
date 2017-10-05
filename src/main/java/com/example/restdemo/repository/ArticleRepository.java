4573ae6aed908faab74188dfb20a39e3e4431ae8
Marcin Bracisiewicz <Marcin.Bracisiewicz@performgroup.com>
2017-10-05 21:59:49 +0200

    rest repository path modified

--- src/main/java/com/example/restdemo/repository/ArticleRepository.java
+++ src/main/java/com/example/restdemo/repository/ArticleRepository.java
@@ -3,12 +3,12 @@ package com.example.restdemo.repository;
 import com.example.restdemo.domain.Article;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by marcin.bracisiewicz
  */
-@RestResource(path = "articles")
+@RepositoryRestResource(path = "arts")
 public interface ArticleRepository extends CrudRepository<Article, Long> {
 
     Article findByTitle(@Param("title") final String title);
